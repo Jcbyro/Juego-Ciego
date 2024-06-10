300f32aea841c9d5ec192175e19339be01916442
Jcbyro <jcbyro@gmail.com>
2024-06-10 18:55:03 -0400

    Version_1.5

--- Juego-Ciego.js
+++ Juego-Ciego.js
@@ -4,7 +4,7 @@ function cafeteria() {
     "Tu puntaje es: " + puntaje;
 
     document.getElementById("texto-ciego").innerHTML =
-    "Despues de pasar por la usual cafeteria con permiso para perros guia vas llendo a tu trabajo. Despues de tomar tu usual cafe vas en camino a tu trabajo, en el camino debes utilizar un medio de transporte, este transporte produce vibraciones cuando este por llegar y se escucha la llegada del transporte por los altavoces. Cual es este medio de transporte?"
+    "<br>Correcto<br><br>Despues de pasar por la usual cafeteria con tu permiso para perros guia, tomas tu usual cafe y vas en camino a tu trabajo, en el camino debes utilizar un medio de transporte, este transporte produce vibraciones cuando este por llegar y se escucha la llegada del transporte por los altavoces. Cual es este medio de transporte?"
 
     document.getElementById("opc1-ciego").removeEventListener("click", cafeteria)    
     document.getElementById("opc2-ciego").removeEventListener("click", parque)
@@ -24,7 +24,7 @@ function parque() {
     "Tu puntaje es: " + puntaje;
 
     document.getElementById("texto-ciego").innerHTML =
-    "Pasar por un parque no es una mala opcion ya que el tranquilo viento te ayuda a relajarte, pero no te ayuda a despertarte tanto que como un buen cafe a la mañana. Despues de tomar un paseo por el parque con tu perro vas en camino a tu trabajo, en el camino debes utilizar un medio de transporte, este transporte produce vibraciones cuando este por llegar y se escucha la llegada del transporte por los altavoces. Cual es este medio de transporte?"
+    "<br>Incorrecto<br><br>Pasar por el parque en bez de tomar un cafe. Despues de tomar un paseo por el parque con tu perro vas en camino a tu trabajo, en el camino debes utilizar un medio de transporte, este transporte produce vibraciones cuando este por llegar y se escucha la llegada del transporte por los altavoces. Cual es este medio de transporte?"
 
     document.getElementById("opc1-ciego").removeEventListener("click", cafeteria)    
     document.getElementById("opc2-ciego").removeEventListener("click", parque)
@@ -44,18 +44,18 @@ function tren() {
     "Tu puntaje es: " + puntaje;
 
     document.getElementById("texto-ciego").innerHTML =
-    "Te bajastes del tren con tu perro, y lograstes llegar a tu trabajo; haces tu usual trabajo y hoy te dejan irte temprano. Decides salir a pasear antes de volver, "
+    "<br>Correcto<br><br>Te bajastes del tren con tu perro, gracias a pasar multiples veces por aca lograstes llegar facilmente a tu trabajo; haces tu usual trabajo y hoy te dejan irte temprano. Decides salir a pasear antes de volver, en este lugar se siente un aire tranquilo, huele a arboles y gente pasa a menudo. Este lugar es?"
 
     document.getElementById("opc1-ciego").removeEventListener("click", tren)
     document.getElementById("opc2-ciego").removeEventListener("click", colectivo)
 
     document.getElementById("opc1-ciego").innerHTML =
-    ""
-    document.getElementById("opc1-ciego").addEventListener("click", )
+    "Un bosque"
+    document.getElementById("opc1-ciego").addEventListener("click", bosque)
 
     document.getElementById("opc2-ciego").innerHTML =
-    ""
-    document.getElementById("opc2-ciego").addEventListener("click", )
+    "Un parque"
+    document.getElementById("opc2-ciego").addEventListener("click", parque_1)
 }
 
 function colectivo() {
@@ -64,60 +64,127 @@ function colectivo() {
     "Tu puntaje es: " + puntaje;
 
     document.getElementById("texto-ciego").innerHTML =
-    ""
+    "<br>Incorrecto<br><br>Esperas a que el colectivo se pare por tercera vez para bajarte, por no usar el tren debes caminar mas de lo normal.<br>Podemos continuar, pero no es la tematica del juego, intenta devuelta."
 
     document.getElementById("opc1-ciego").removeEventListener("click", tren)
     document.getElementById("opc2-ciego").removeEventListener("click", colectivo)
 
     document.getElementById("opc1-ciego").innerHTML =
-    ""
-    document.getElementById("opc1-ciego").addEventListener("click", )
-
-    document.getElementById("opc2-ciego").innerHTML =
-    ""
-    document.getElementById("opc2-ciego").addEventListener("click", )
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function volver_casa() {
-    document.getElementById("puntaje-ciego").innerHTML =
-    "Tu puntaje es: " + puntaje;
-
-    if(puntaje == 10) 
-        document.getElementById("texto-ciego").innerHTML =
-        "<strong>¡Perfecto!</strong><br>Obtuviste el puntaje más alto posible.<br>Has tomado las decisiones correctas en las situaciones que se plantearon. El objetivo de esta actividad es de mostrar las dificultades del día a día de una persona ciega que pueden pasar desapercibidas para muchas personas.<br>Esto es solo una mínima porción de la realidad a la que se enfrentan.<br>Qué pasaría si no hay otra opción mas que caminar hasta el parque o no es posible pedir un taxi o un amigo?<br><br>Estas con cuestiones que como sociedad deberíamos considerarlas para actuar en consecuencia y asegurar que todo, desde transporte, calles, accesos a lugares sean accesibles para todos.<br><br>Muchas gracias."
-    else if(puntaje == 5)
-        document.getElementById("texto-ciego").innerHTML =
-        "<strong>¡Muy Bien!</strong><br>Obtuviste un buen puntaje.<br>Esperamos que este juego te haya ayudado a apreciar los desafíos que existen, pero quizás se nos pasan por alto. El objetivo de esta actividad es de mostrar las dificultades del día a día ciega de ruedas que pueden pasar desapercibidas para muchas personas.<br>Esto es solo una mínima porción de la realidad a la que se enfrentan.<br>Qué pasaría si no hay otra opción mas que caminar hasta el parque o no es posible pedir un taxi o un amigo?<br><br>Estas con cuestiones que como sociedad deberíamos considerarlas para actuar en consecuencia y asegurar que todo, desde transporte, calles, accesos a lugares sean accesibles para todos.<br><br>Muchas gracias."
-    else 
-        document.getElementById("texto-ciego").innerHTML =
-        "Esperamos que este juego te haya ayudado a apreciar los desafíos que existen, pero quizás se nos pasan por alto. El objetivo de esta actividad es de mostrar las dificultades del día a día ciega de ruedas que pueden pasar desapercibidas para muchas personas.<br>Esto es solo una mínima porción de la realidad a la que se enfrentan.<br>Qué pasaría si no hay otra opción mas que caminar hasta el parque o no es posible pedir un taxi o un amigo?<br><br>Estas con cuestiones que como sociedad deberíamos considerarlas para actuar en consecuencia y asegurar que todo, desde transporte, calles, accesos a lugares sean accesibles para todos.<br><br>Muchas gracias."
-
-    document.getElementById("opc1-ciego").innerHTML =
-    "Volver a Home"
+    "Reiniciar"
     document.getElementById("opc1-ciego").addEventListener("click", function() {
-        window.location.href = "Index.html";
+        window.location.href = "index.html";
     });
     
     document.getElementById("opc2-ciego").innerHTML =
-    "Volver a Juegos"
+    "Reiniciar"
     document.getElementById("opc2-ciego").addEventListener("click", function() {
-        window.location.href = "Juegos.html";
+        window.location.href = "index.html";
+    });
+}
+
+function parque_1() {
+    puntaje += 5;
+    document.getElementById("puntaje-ciego").innerHTML =
+    "Tu puntaje es: " + puntaje;
+
+    document.getElementById("texto-ciego").innerHTML =
+    "<br>Correcto<br><br>Decides ir a un parque, el aire refrescante te ayuda a disfrutar del paseo y gracias a que el parque esta padimentado, las chances de que caigas disminuyen; para finalizar el dia decides ir a un lugar de entretenimiento, en este lugar se puede escuchar las actuaciones que ocurren y los murmullos de la audiencia, este lugar es un:"
+
+    document.getElementById("opc1-ciego").removeEventListener("click", bosque)
+    document.getElementById("opc2-ciego").removeEventListener("click", parque_1)
+
+    document.getElementById("opc1-ciego").innerHTML =
+    "Musical"
+    document.getElementById("opc1-ciego").addEventListener("click", musical)
+
+    document.getElementById("opc2-ciego").innerHTML =
+    "Teatro"
+    document.getElementById("opc2-ciego").addEventListener("click", teatro)
+}
+
+function bosque() {
+    puntaje -= 5;
+    document.getElementById("puntaje-ciego").innerHTML =
+    "Tu puntaje es: " + puntaje;
+
+    document.getElementById("texto-ciego").innerHTML =
+    "<br>Incorrecto<br><br>Decides ir a un bosque, el aire refrescante te ayuda a disfrutar del paseo, pero las chances de que caigas aumentan en la presencia de ramas o troncos en el piso; para finalizar el dia decides ir a un lugar de entretenimiento, en este lugar se puede escuchar las actuaciones que ocurren y los murmullos de la audiencia, este lugar es un:"
+
+    document.getElementById("opc1-ciego").removeEventListener("click", bosque)
+    document.getElementById("opc2-ciego").removeEventListener("click", parque_1)
+
+    document.getElementById("opc1-ciego").innerHTML =
+    "Musical"
+    document.getElementById("opc1-ciego").addEventListener("click", musical)
+
+    document.getElementById("opc2-ciego").innerHTML =
+    "Teatro"
+    document.getElementById("opc2-ciego").addEventListener("click", teatro)
+}
+
+function teatro() {
+    puntaje += 5;
+    document.getElementById("puntaje-ciego").innerHTML =
+    "Tu puntaje es: " + puntaje;
+
+    document.getElementById("texto-ciego").innerHTML =
+    "<br>Correcto<br><br>El teatro es la opcion correcta ya que en un teatro lo que reina son las actuaciones. Despues de entretenerte decides volver a casa para relajarte."
+
+    document.getElementById("opc1-ciego").removeEventListener("click", musical)
+    document.getElementById("opc2-ciego").removeEventListener("click", teatro)
+
+    document.getElementById("opc1-ciego").innerHTML =
+    "Volver a casa"
+    document.getElementById("opc1-ciego").addEventListener("click", volver)
+
+    document.getElementById("opc2-ciego").innerHTML =
+    "Volver a casa"
+    document.getElementById("opc2-ciego").addEventListener("click", volver)
+}
+
+function musical() {
+    puntaje -= 5;
+    document.getElementById("puntaje-ciego").innerHTML =
+    "Tu puntaje es: " + puntaje;
+
+    document.getElementById("texto-ciego").innerHTML =
+    "<br>Incorrecto<br><br>Un musical no es una mala opcion, pero en un musical lo que reina es la musica mas que la actuacion. Despues de entretenerte decides volver a casa para relajarte."
+
+    document.getElementById("opc1-ciego").removeEventListener("click", musical)
+    document.getElementById("opc2-ciego").removeEventListener("click", teatro)
+
+    document.getElementById("opc1-ciego").innerHTML =
+    "Volver a casa"
+    document.getElementById("opc1-ciego").addEventListener("click", volver)
+
+    document.getElementById("opc2-ciego").innerHTML =
+    "Volver a casa"
+    document.getElementById("opc2-ciego").addEventListener("click", volver)
+}
+
+function volver() {
+    document.getElementById("puntaje-ciego").innerHTML =
+    "Tu puntaje final es: " + puntaje;
+
+    if(puntaje == 30) 
+        document.getElementById("texto-ciego").innerHTML =
+        "<br><strong>¡Perfecto!</strong><br><br>Obtuviste el puntaje más alto posible.<br><br>Este juego fue diseñado con el propósito de resaltar la importancia de la inclusión social de las personas ciegas. Busca simular la experiencia de vida de una persona con ceguera, adaptándose a los sonidos del entorno para determinar su ubicación. Sin embargo, más allá de la adaptación individual, este juego enfatiza el papel crucial que juega la sociedad en apoyar a las personas con discapacidades visuales en su orientación diaria. Aunque cuenten con herramientas como un bastón y un perro guía, su percepción del mundo se basa principalmente en el oído. Por lo tanto, es responsabilidad de todos facilitar su integración y participación en la sociedad, reafirmando que la inclusión social de las personas ciegas es un compromiso colectivo.<br><br>Muchas gracias por jugar este juego."
+    else if(puntaje >= 10 && puntaje <= 25)
+        document.getElementById("texto-ciego").innerHTML =
+        "<br><strong>¡Muy Bien!</strong><br><br>Obtuviste un buen puntaje.<br><br>Este juego fue diseñado con el propósito de resaltar la importancia de la inclusión social de las personas ciegas. Busca simular la experiencia de vida de una persona con ceguera, adaptándose a los sonidos del entorno para determinar su ubicación. Sin embargo, más allá de la adaptación individual, este juego enfatiza el papel crucial que juega la sociedad en apoyar a las personas con discapacidades visuales en su orientación diaria. Aunque cuenten con herramientas como un bastón y un perro guía, su percepción del mundo se basa principalmente en el oído. Por lo tanto, es responsabilidad de todos facilitar su integración y participación en la sociedad, reafirmando que la inclusión social de las personas ciegas es un compromiso colectivo.<br><br>Muchas gracias por jugar este juego."
+    else 
+        document.getElementById("texto-ciego").innerHTML =
+        "<br><strong>¡Mal Puntuaje!</strong><br><br>Obtuviste un puntuaje bajo.<br><br>Este juego fue diseñado con el propósito de resaltar la importancia de la inclusión social de las personas ciegas. Busca simular la experiencia de vida de una persona con ceguera, adaptándose a los sonidos del entorno para determinar su ubicación. Sin embargo, más allá de la adaptación individual, este juego enfatiza el papel crucial que juega la sociedad en apoyar a las personas con discapacidades visuales en su orientación diaria. Aunque cuenten con herramientas como un bastón y un perro guía, su percepción del mundo se basa principalmente en el oído. Por lo tanto, es responsabilidad de todos facilitar su integración y participación en la sociedad, reafirmando que la inclusión social de las personas ciegas es un compromiso colectivo.<br><br>Muchas gracias por jugar este juego."
+
+    document.getElementById("opc1-ciego").innerHTML =
+    "Reiniciar"
+    document.getElementById("opc1-ciego").addEventListener("click", function() {
+        window.location.href = "index.html";
+    });
+        
+    document.getElementById("opc2-ciego").innerHTML =
+    "Reiniciar"
+    document.getElementById("opc2-ciego").addEventListener("click", function() {
+        window.location.href = "index.html";
     });
 }
