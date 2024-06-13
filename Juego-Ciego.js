c2886e2a903e28b7da0cf361132d9a026d6728b3
Jcbyro <jcbyro@gmail.com>
2024-06-12 20:09:20 -0400

    Version_2.0

--- Juego-Ciego.js
+++ Juego-Ciego.js
@@ -1,5 +1,5 @@
 // Computadora
-function cafeteria() {
+function cafeteria_compu() {
     puntaje += 10;
     document.getElementById("puntaje-ciego-compu").innerHTML =
     "Tu puntaje es: " + puntaje;
@@ -7,19 +7,19 @@ function cafeteria() {
     document.getElementById("texto-ciego-compu").innerHTML =
     "<br>Correcto<br><br>Despues de pasar por la usual cafeteria con tu permiso para perros guia, tomas tu usual cafe y vas en camino a tu trabajo, en el camino debes utilizar un medio de transporte, este transporte produce vibraciones cuando este por llegar y se escucha la llegada del transporte por los altavoces. Cual es este medio de transporte?"
 
-    document.getElementById("opc1-ciego-compu").removeEventListener("click", cafeteria)    
-    document.getElementById("opc2-ciego-compu").removeEventListener("click", parque)
+    document.getElementById("opc1-ciego-compu").removeEventListener("click", cafeteria_compu)    
+    document.getElementById("opc2-ciego-compu").removeEventListener("click", parque_compu)
 
     document.getElementById("opc1-ciego-compu").innerHTML =
     "Un tren"
-    document.getElementById("opc1-ciego-compu").addEventListener("click", tren)
+    document.getElementById("opc1-ciego-compu").addEventListener("click", tren_compu)
 
     document.getElementById("opc2-ciego-compu").innerHTML =
     "Un colectivo"
-    document.getElementById("opc2-ciego-compu").addEventListener("click", colectivo)
+    document.getElementById("opc2-ciego-compu").addEventListener("click", colectivo_compu)
 }
 
-function parque() {
+function parque_compu() {
     puntaje -= 10;
     document.getElementById("puntaje-ciego-compu").innerHTML =
     "Tu puntaje es: " + puntaje;
@@ -27,19 +27,19 @@ function parque() {
     document.getElementById("texto-ciego-compu").innerHTML =
     "<br>Incorrecto<br><br>Pasar por el parque en bez de tomar un cafe. Despues de tomar un paseo por el parque con tu perro vas en camino a tu trabajo, en el camino debes utilizar un medio de transporte, este transporte produce vibraciones cuando este por llegar y se escucha la llegada del transporte por los altavoces. Cual es este medio de transporte?"
 
-    document.getElementById("opc1-ciego-compu").removeEventListener("click", cafeteria)    
-    document.getElementById("opc2-ciego-compu").removeEventListener("click", parque)
+    document.getElementById("opc1-ciego-compu").removeEventListener("click", cafeteria_compu)    
+    document.getElementById("opc2-ciego-compu").removeEventListener("click", parque_compu)
 
     document.getElementById("opc1-ciego-compu").innerHTML =
     "Un tren"
-    document.getElementById("opc1-ciego-compu").addEventListener("click", tren)
+    document.getElementById("opc1-ciego-compu").addEventListener("click", tren_compu)
 
     document.getElementById("opc2-ciego-compu").innerHTML =
     "Un colectivo"
-    document.getElementById("opc2-ciego-compu").addEventListener("click", colectivo)
+    document.getElementById("opc2-ciego-compu").addEventListener("click", colectivo_compu)
 }
 
-function tren() {
+function tren_compu() {
     puntaje += 10;
     document.getElementById("puntaje-ciego-compu").innerHTML =
     "Tu puntaje es: " + puntaje;
@@ -47,19 +47,19 @@ function tren() {
     document.getElementById("texto-ciego-compu").innerHTML =
     "<br>Correcto<br><br>Te bajastes del tren con tu perro, gracias a pasar multiples veces por aca lograstes llegar facilmente a tu trabajo; haces tu usual trabajo y hoy te dejan irte temprano. Decides salir a pasear antes de volver, en este lugar se siente un aire tranquilo, huele a arboles y gente pasa a menudo. Este lugar es?"
 
-    document.getElementById("opc1-ciego-compu").removeEventListener("click", tren)
-    document.getElementById("opc2-ciego-compu").removeEventListener("click", colectivo)
+    document.getElementById("opc1-ciego-compu").removeEventListener("click", tren_compu)
+    document.getElementById("opc2-ciego-compu").removeEventListener("click", colectivo_compu)
 
     document.getElementById("opc1-ciego-compu").innerHTML =
     "Un bosque"
-    document.getElementById("opc1-ciego-compu").addEventListener("click", bosque)
+    document.getElementById("opc1-ciego-compu").addEventListener("click", bosque_compu)
 
     document.getElementById("opc2-ciego-compu").innerHTML =
     "Un parque"
-    document.getElementById("opc2-ciego-compu").addEventListener("click", parque_1)
+    document.getElementById("opc2-ciego-compu").addEventListener("click", parque_1_compu)
 }
 
-function colectivo() {
+function colectivo_compu() {
     puntaje -= 10;
     document.getElementById("puntaje-ciego-compu").innerHTML =
     "Tu puntaje es: " + puntaje;
@@ -67,8 +67,8 @@ function colectivo() {
     document.getElementById("texto-ciego-compu").innerHTML =
     "<br>Incorrecto<br><br>Esperas a que el colectivo se pare por tercera vez para bajarte, por no usar el tren debes caminar mas de lo normal.<br>Podemos continuar, pero no es la tematica del juego, intenta devuelta."
 
-    document.getElementById("opc1-ciego-compu").removeEventListener("click", tren)
-    document.getElementById("opc2-ciego-compu").removeEventListener("click", colectivo)
+    document.getElementById("opc1-ciego-compu").removeEventListener("click", tren_compu)
+    document.getElementById("opc2-ciego-compu").removeEventListener("click", colectivo_compu)
 
     document.getElementById("opc1-ciego-compu").innerHTML =
     "Reiniciar"
@@ -83,7 +83,7 @@ function colectivo() {
     });
 }
 
-function parque_1() {
+function parque_1_compu() {
     puntaje += 5;
     document.getElementById("puntaje-ciego-compu").innerHTML =
     "Tu puntaje es: " + puntaje;
@@ -91,19 +91,19 @@ function parque_1() {
     document.getElementById("texto-ciego-compu").innerHTML =
     "<br>Correcto<br><br>Decides ir a un parque, el aire refrescante te ayuda a disfrutar del paseo y gracias a que el parque esta padimentado, las chances de que caigas disminuyen; para finalizar el dia decides ir a un lugar de entretenimiento, en este lugar se puede escuchar las actuaciones que ocurren y los murmullos de la audiencia, este lugar es un:"
 
-    document.getElementById("opc1-ciego-compu").removeEventListener("click", bosque)
-    document.getElementById("opc2-ciego-compu").removeEventListener("click", parque_1)
+    document.getElementById("opc1-ciego-compu").removeEventListener("click", bosque_compu)
+    document.getElementById("opc2-ciego-compu").removeEventListener("click", parque_1_compu)
 
     document.getElementById("opc1-ciego-compu").innerHTML =
     "Musical"
-    document.getElementById("opc1-ciego-compu").addEventListener("click", musical)
+    document.getElementById("opc1-ciego-compu").addEventListener("click", musical_compu)
 
     document.getElementById("opc2-ciego-compu").innerHTML =
     "Teatro"
-    document.getElementById("opc2-ciego-compu").addEventListener("click", teatro)
+    document.getElementById("opc2-ciego-compu").addEventListener("click", teatro_compu)
 }
 
-function bosque() {
+function bosque_compu() {
     puntaje -= 5;
     document.getElementById("puntaje-ciego-compu").innerHTML =
     "Tu puntaje es: " + puntaje;
@@ -111,19 +111,19 @@ function bosque() {
     document.getElementById("texto-ciego-compu").innerHTML =
     "<br>Incorrecto<br><br>Decides ir a un bosque, el aire refrescante te ayuda a disfrutar del paseo, pero las chances de que caigas aumentan en la presencia de ramas o troncos en el piso; para finalizar el dia decides ir a un lugar de entretenimiento, en este lugar se puede escuchar las actuaciones que ocurren y los murmullos de la audiencia, este lugar es un:"
 
-    document.getElementById("opc1-ciego-compu").removeEventListener("click", bosque)
-    document.getElementById("opc2-ciego-compu").removeEventListener("click", parque_1)
+    document.getElementById("opc1-ciego-compu").removeEventListener("click", bosque_compu)
+    document.getElementById("opc2-ciego-compu").removeEventListener("click", parque_1_compu)
 
     document.getElementById("opc1-ciego-compu").innerHTML =
     "Musical"
-    document.getElementById("opc1-ciego-compu").addEventListener("click", musical)
+    document.getElementById("opc1-ciego-compu").addEventListener("click", musical_compu)
 
     document.getElementById("opc2-ciego-compu").innerHTML =
     "Teatro"
-    document.getElementById("opc2-ciego-compu").addEventListener("click", teatro)
+    document.getElementById("opc2-ciego-compu").addEventListener("click", teatro_compu)
 }
 
-function teatro() {
+function teatro_compu() {
     puntaje += 5;
     document.getElementById("puntaje-ciego-compu").innerHTML =
     "Tu puntaje es: " + puntaje;
@@ -131,19 +131,19 @@ function teatro() {
     document.getElementById("texto-ciego-compu").innerHTML =
     "<br>Correcto<br><br>El teatro es la opcion correcta ya que en un teatro lo que reina son las actuaciones. Despues de entretenerte decides volver a casa para relajarte."
 
-    document.getElementById("opc1-ciego-compu").removeEventListener("click", musical)
-    document.getElementById("opc2-ciego-compu").removeEventListener("click", teatro)
+    document.getElementById("opc1-ciego-compu").removeEventListener("click", musical_compu)
+    document.getElementById("opc2-ciego-compu").removeEventListener("click", teatro_compu)
 
     document.getElementById("opc1-ciego-compu").innerHTML =
     "Volver a casa"
-    document.getElementById("opc1-ciego-compu").addEventListener("click", volver)
+    document.getElementById("opc1-ciego-compu").addEventListener("click", volver_compu)
 
     document.getElementById("opc2-ciego-compu").innerHTML =
     "Volver a casa"
-    document.getElementById("opc2-ciego-compu").addEventListener("click", volver)
+    document.getElementById("opc2-ciego-compu").addEventListener("click", volver_compu)
 }
 
-function musical() {
+function musical_compu() {
     puntaje -= 5;
     document.getElementById("puntaje-ciego-compu").innerHTML =
     "Tu puntaje es: " + puntaje;
@@ -151,19 +151,19 @@ function musical() {
     document.getElementById("texto-ciego-compu").innerHTML =
     "<br>Incorrecto<br><br>Un musical no es una mala opcion, pero en un musical lo que reina es la musica mas que la actuacion. Despues de entretenerte decides volver a casa para relajarte."
 
-    document.getElementById("opc1-ciego-compu").removeEventListener("click", musical)
-    document.getElementById("opc2-ciego-compu").removeEventListener("click", teatro)
+    document.getElementById("opc1-ciego-compu").removeEventListener("click", musical_compu)
+    document.getElementById("opc2-ciego-compu").removeEventListener("click", teatro_compu)
 
     document.getElementById("opc1-ciego-compu").innerHTML =
     "Volver a casa"
-    document.getElementById("opc1-ciego-compu").addEventListener("click", volver)
+    document.getElementById("opc1-ciego-compu").addEventListener("click", volver_compu)
 
     document.getElementById("opc2-ciego-compu").innerHTML =
     "Volver a casa"
-    document.getElementById("opc2-ciego-compu").addEventListener("click", volver)
+    document.getElementById("opc2-ciego-compu").addEventListener("click", volver_compu)
 }
 
-function volver() {
+function volver_compu() {
     document.getElementById("puntaje-ciego-compu").innerHTML =
     "Tu puntaje final es: " + puntaje;
 
